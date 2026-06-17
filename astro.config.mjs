defa608ff8e11ea4860c07982db00eceed3ceade
Daniel <drrichardcheung@gmail.com>
2026-06-17 11:13:31 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://danielcheung.github.io',
+  base: '/SimpleStockWorker/',
   output: 'static',
   build: {
     assets: 'assets'
